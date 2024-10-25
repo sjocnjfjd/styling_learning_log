7d5bc408eed693de2d95690ba7018a64f1a805e7
东方饭店 <3596335798@qq.com>
2024-10-25 10:41:05 +0800

    修改 settings.py，从环境变量中读取 DEBUG 设置

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mibxt7sboxg%r)9t6z=a6@weyo(thgbknfcwgyowgx9$5s4&=!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
